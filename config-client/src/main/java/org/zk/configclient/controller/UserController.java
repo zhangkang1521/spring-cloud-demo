bc5f695777dd97ea784325e6ca33d6fff6b8896b
zhangkang <zhangkang@lvmama.com>
2020-04-02 17:07:03 +0800

    config 配置中心

---
+++ config-client/src/main/java/org/zk/configclient/controller/UserController.java
@@ -0,0 +1,17 @@
+package org.zk.configclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+	@Value("${foo}")
+	String foo;
+
+	@RequestMapping(value = "/hi")
+	public String hi() {
+		return foo;
+	}
+}
---
+++ config-server/src/main/java/org/zk/configserver/ConfigServerApplication.java
@@ -0,0 +1,18 @@
+package org.zk.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * http://localhost:8888/user-service/prod
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
