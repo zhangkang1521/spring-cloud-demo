059e77f209446e0bc0820f644b8fb13573579734
zhangkang <zhangkang@lvmama.com>
2020-04-08 14:55:59 +0800

    config-client 使用bootstrap.properties

--- config-server/src/main/java/org/zk/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/org/zk/configserver/ConfigServerApplication.java
@@ -3,12 +3,14 @@ package org.zk.configserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * http://localhost:8888/user-service/prod
  */
 @SpringBootApplication
 @EnableConfigServer
+//@EnableEurekaClient
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
