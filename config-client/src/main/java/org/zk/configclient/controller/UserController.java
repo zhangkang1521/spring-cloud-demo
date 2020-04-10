d2df8be071b650ebcbbf27e4eebda608143ba454
zhangkang <zhangkang@lvmama.com>
2020-04-10 15:58:24 +0800

    消息总线-修改配置无需重启

--- config-client/src/main/java/org/zk/configclient/controller/UserController.java
+++ config-client/src/main/java/org/zk/configclient/controller/UserController.java
@@ -1,10 +1,12 @@
 package org.zk.configclient.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class UserController {
 
 	@Value("${foo}")
@@ -12,6 +14,7 @@ public class UserController {
 
 	@RequestMapping(value = "/hi")
 	public String hi() {
+		System.out.println(this);
 		return foo;
 	}
 }
