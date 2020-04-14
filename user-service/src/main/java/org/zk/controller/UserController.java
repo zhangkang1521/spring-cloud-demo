73a69d76a07f80ac9d83bb0311bc367d9efbcd4a
zhangkang <zhangkang@lvmama.com>
2020-04-14 16:23:48 +0800

    hystrix-turbine 断路器聚合监控

--- user-service/src/main/java/org/zk/controller/UserController.java
+++ user-service/src/main/java/org/zk/controller/UserController.java
@@ -1,13 +1,23 @@
 package org.zk.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@EnableHystrix
+@EnableHystrixDashboard
 public class UserController {
 
 	@RequestMapping("/user")
+	@HystrixCommand(fallbackMethod = "hiError")
 	public String user() {
 		return "Hi,I am user-service";
+	}
+
+	public String hiError() {
+		return "hi,sorry,error!";
 	}
 }
